210051a51ea23b0310379618c862c66b61a1c7a6
oliviabreda <livbreda@gmail.com>
2022-04-05 21:25:11 -0400

    Player class partial

--- Player.java
+++ Player.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.lang.*;
 public class Player {
+    private String name;
+    private int score;
+    private boolean control;
+
+    Player (String name){
+        this.name=name;
+    }
+    public void addPoints(int p){
+        score=score+p;
+    }
+    public void subtractPoints(int p){
+        score=score-p;
+    }
+
 }
