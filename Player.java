de549b6f480cab1c5909698a5b1cf22550162591
nikt1357 <gtoro@mail.umw.edu>
2022-04-05 21:32:12 -0400

    Formatted Player.java class

--- Player.java
+++ Player.java
@@ -1,19 +1,23 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.lang.*;
+
 public class Player {
+
     private String name;
     private int score;
     private boolean control;
 
-    Player (String name){
+    public Player(String name){
         this.name=name;
     }
+
     public void addPoints(int p){
-        score=score+p;
+        score = score + p;
     }
+
     public void subtractPoints(int p){
-        score=score-p;
+        score = score - p;
     }
 
 }
