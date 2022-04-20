9d02ce3c1cebe09db7aee2b527db44406fcc0ea5
oliviabreda <livbreda@gmail.com>
2022-04-20 02:38:02 -0400

    result window works +changed font getWinner doesnt account for ties, crashes

--- src/com/company/ResultWindow.java
+++ src/com/company/ResultWindow.java
@@ -7,68 +7,192 @@ import java.util.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.event.*;
+public class ResultWindow extends Thread {
 
-public class ResultWindow extends Thread{
-    ArrayList<Player> players;
+    private Player p1;
+    private Player p2;
+    private Player p3;
 
     public ResultWindow(ArrayList<Player> players) {
-        this.players=players;
+        p1 = players.get(0);
+        p2 = players.get(1);
+        p3 = players.get(2);
     }
 
-    JLabel label;
-    //Figure out which player has the highest amount
-    public void run(){
-        Player p1;
-        Player p2;
-        Player p3;
-
-        int s1= players.get(0).getScore();
-        int s2= players.get(1).getScore();
-        int s3= players.get(2).getScore();
-
-
-        JFrame frame=new JFrame("The winner is...");
-        if ((s1>s2)&&(s1>s3)){
-            label= new JLabel(players.get(0).getName()+" Wins with " +s1+" points! Congrats");
-        }
-        if ((s2>s1)&&(s2>s3)){
-            label= new JLabel(players.get(1).getName()+" Wins! with " +s2+" points! Congrats");
-        }
-        if ((s3>s1)&&(s3>s2)){
-            label= new JLabel(players.get(2).getName()+" Wins! with " +s3+" points! Congrats");
-        }
-        if ((s3>s1)&&(s3==s2)){
-            label= new JLabel(players.get(2).getName()+" and "+players.get(1).getName()+" have tied for the win with "+s3+" points! Congrats");
-        }
-        if ((s3>s2)&&(s3==s1)){
-            label= new JLabel(players.get(2).getName()+" and "+players.get(0).getName()+" have tied for the win with "+s3+" points! Congrats");
-        }
-        if ((s3==s1)&&(s3==s2)){
-            label= new JLabel(players.get(2).getName()+" and "+players.get(1).getName()+" and "+players.get(0).getName()+" have tied for the win with "+s3+" points! Congrats");
-        }
-        if ((s2>s3)&&(s1==s2)){
-            label= new JLabel(players.get(0).getName()+" and "+players.get(1).getName()+" have tied for the win with "+s1+" points! Congrats");
-        }
+    public void run() {
 
         Border blackLine = BorderFactory.createLineBorder(Color.black);
-        label.setBorder(blackLine);
-        label.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalAlignment(SwingConstants.CENTER);
+        Border blank = BorderFactory.createEmptyBorder(10, 10, 10, 10);
 
+        JFrame results = new JFrame("And the winner is....");
+        results.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.setBorder(blackLine);
+        panel.setPreferredSize(new Dimension(100,100));
 
-        frame.add(label).setSize(500,30);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setSize(500, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel congrats = new JLabel("Congratulations!!!", SwingConstants.CENTER);
+        congrats.setVerticalAlignment(SwingConstants.CENTER);
+        congrats.setBorder(blank);
+        panel.add(congrats);
 
+        JLabel name = new JLabel(getWinner().getName(), SwingConstants.CENTER);
+        name.setVerticalAlignment(SwingConstants.CENTER);
+        name.setBorder(blank);
+        panel.add(name);
+
+        JLabel score = new JLabel(String.valueOf(getWinner().getScore()), SwingConstants.CENTER);
+        score.setVerticalAlignment(SwingConstants.CENTER);
+        score.setBorder(blank);
+        panel.add(score);
+
+        congrats.setAlignmentX(Box.CENTER_ALIGNMENT);
+        congrats.setFont(new Font("Times New Roman", Font.BOLD, 15));
+        name.setAlignmentX(Box.CENTER_ALIGNMENT);
+        name.setFont(new Font("Times New Roman", Font.BOLD, 15));
+        score.setAlignmentX(Box.CENTER_ALIGNMENT);
+        score.setFont(new Font("Times New Roman", Font.BOLD, 15));
+
+        results.getContentPane().add(new GameWorld(250));
+        results.setSize(500,500);
+        results.setLocationRelativeTo(null);
+        results.setVisible(true);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+        }
+        UIManager.put("OptionPane.minimumSize", new Dimension(300,300));
+        JOptionPane.showMessageDialog(results, panel);
     }
 
-    //Print out congratulation message in window with player's name
-    //Confetti?
+    public Player getWinner() {
 
-    //Exit listener
+        int points1 = p1.getScore();
+        int points2 = p2.getScore();
+        int points3 = p3.getScore();
+        Player winner = null;
 
+        if ((points1 > points2) && (points1 > points3)) {
+            winner = p1;
+        } else if ((points2 > points1) && (points2 > points3)) {
+            winner = p2;
+        } else if ((points3 > points1) && (points3 > points2)) {
+            winner = p3;
+        }
+
+
+        return winner;
+    }
+
+    class Confetti {
+
+        private double x, y, dx, dy;
+
+        public Confetti() {
+            Random r = new Random();
+            x = r.nextFloat() * 500;
+            y = r.nextFloat() * 500;
+
+            dx = r.nextFloat() * 50 - 25;
+            dy = r.nextFloat() * 50 + 100;
+        }
+
+        public void draw(Graphics g) {
+            g.fillRect((int) x, (int) y, 5, 5);
+        }
+
+        public void update(double dt) {
+            x += (dx * dt);
+            y += (dy * dt);
+
+            if (y < 0) {
+                y = 500;
+            }
+            if (y > 500) {
+                y = 0;
+            }
+            if (x < 0) {
+                x = 500;
+            }
+            if (x > 500) {
+                x = 0;
+            }
+        }
+    }
+
+    public class GameWorld extends JComponent {
+
+        private ArrayList<Confetti> yellow;
+        private ArrayList<Confetti> red;
+        private ArrayList<Confetti> green;
+        private ArrayList<Confetti> blue;
+        private long last_time;
+
+        public GameWorld(int count) {
+            last_time = new Date().getTime();
+
+            yellow = new ArrayList<>(count/4);
+            for (int i = 0; i < count; i++) {
+                yellow.add(new Confetti());
+            }
+            red = new ArrayList<>(count/4);
+            for (int i = 0; i < count; i++) {
+                red.add(new Confetti());
+            }
+            green = new ArrayList<>(count/4);
+            for (int i = 0; i < count; i++) {
+                green.add(new Confetti());
+            }
+            blue = new ArrayList<>(count/4);
+            for (int i = 0; i < count; i++) {
+                blue.add(new Confetti());
+            }
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0,0,500,500);
+
+            g.setColor(Color.yellow);
+            for (Confetti c : yellow) {
+                c.draw(g);
+            }
+            g.setColor(Color.red);
+            for (Confetti c : red) {
+                c.draw(g);
+            }
+            g.setColor(Color.green);
+            for (Confetti c : green) {
+                c.draw(g);
+            }
+            g.setColor(Color.blue);
+            for (Confetti c : blue) {
+                c.draw(g);
+            }
+
+            long time_now = new Date().getTime();
+            long elapsed_ms = time_now - last_time;
+            double dt = elapsed_ms / 1000.0;
+
+            for (Confetti c : yellow) {
+                c.update(dt);
+            }
+            for (Confetti c : red) {
+                c.update(dt);
+            }
+            for (Confetti c : green) {
+                c.update(dt);
+            }
+            for (Confetti c : blue) {
+                c.update(dt);
+            }
+
+            last_time = time_now;
+            revalidate();
+            repaint();
+        }
+    }
 }
