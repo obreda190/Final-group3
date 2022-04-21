fbb4bbf0a6809f4b859498dbafdd9d146238f382
nikt1357 <gtoro@mail.umw.edu>
2022-04-20 23:37:04 -0400

    Javadoc comments written for DWindow.java class

--- src/com/company/DWindow.java
+++ src/com/company/DWindow.java
@@ -7,33 +7,52 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.util.*;
 
+/**
+ * @author Niko Toro, Grace Ordonez, Olivia Breda
+ * This class creates a JFrame containing 6 random categories and their respective questions displayed as a grid of JButtons.
+ */
 public class DWindow extends Thread {
 
-    private ArrayList<Category> categories;
+    private final ArrayList<Category> categories;
     private ArrayList<Player> players;
-    private JFrame frame;
 
+    /**
+     * Non-default constructor that takes an ArrayLists containing 6 random categories to be displayed in the game.
+     * @param categories ArrayList that contains the Category objects to be used for the Double Jeopardy round
+     */
     public DWindow(ArrayList<Category> categories) {
         this.categories = categories;
     }
 
+    /**
+     * Setter method that instantiates an ArrayList of the current players into the DWindow.
+     * @param players ArrayList containing the Player objects that represent the current players
+     */
     public void setPlayers(ArrayList<Player> players) {
         this.players = players;
     }
 
+    /**
+     * Overridden run method that sets up and displays the JFrame and Timer to be used in the Double Jeopardy round.
+     * Upon completion, the JFrame is disposed of and a new ResultWindow is created and started.
+     */
+    @Override
     public void run() {
 
+        // Borders used in JFrame layout
         Border blackLine = BorderFactory.createLineBorder(Color.black);
         Border blank = BorderFactory.createEmptyBorder(10, 10, 10, 10);
 
+        // Timer and new JFrame created and formatted
         Timers t1 = new Timers(RoundType.Jeopardy);
 
-        frame = new JFrame("Double Jeopardy Round");
+        JFrame frame = new JFrame("Double Jeopardy Round");
         frame.setForeground(Color.white);
         frame.setBackground(Color.blue);
         frame.getRootPane().setBorder(blank);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // For loop that iterates through the Categories used for the current round and adding them into their own JPanel
         for (Category c : categories) {
 
             c.doubleJeopardy();
@@ -41,6 +60,7 @@ public class DWindow extends Thread {
             JPanel panel = new JPanel();
             panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
+            // Formatting the JLabel representing the Category object
             JLabel cat = new JLabel("<html><p>" + c.getName() + "</p></html>", SwingConstants.CENTER);
             cat.setVerticalAlignment(SwingConstants.CENTER);
             cat.setBorder(blank);
@@ -48,11 +68,14 @@ public class DWindow extends Thread {
             panel.add(cat);
             cat.setAlignmentX(Box.CENTER_ALIGNMENT);
 
+            // For loop that iterates through the Questions in a respective Category and adding them as JButtons into
+            // their Category's JPanel
             for (Question q : questions) {
 
                 JPanel p = new JPanel();
                 p.setLayout(new BorderLayout());
 
+                // Formatting the JButton representing the Question object and establishing ButtonListeners
                 JButton button = new JButton(q.pointString());
                 button.addActionListener(new ButtonListener(q, button));
                 p.add(button, BorderLayout.CENTER);
@@ -60,6 +83,7 @@ public class DWindow extends Thread {
                 panel.add(p);
             }
 
+            // Formatting JPanel and adding it to the JFrame
             panel.setPreferredSize(new Dimension(210, 500));
             panel.setBorder(blackLine);
             panel.setVisible(true);
@@ -67,11 +91,13 @@ public class DWindow extends Thread {
             frame.getContentPane().add(panel);
         }
 
+        // JFrame packed and displayed
         frame.getContentPane().setLayout(new FlowLayout());
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
+        // Round timer begins after the JFrame is displayed, JFrame is made invisible once timer ends
         t1.start();
         try {
             t1.join();
@@ -80,24 +106,40 @@ public class DWindow extends Thread {
         }
         frame.setVisible(false);
 
+        // JFrame representing Single Jeopardy is disposed of after creating and running a new ResultWindow object
         ResultWindow rw = new ResultWindow(players);
         rw.start();
         frame.dispose();
     }
 
+    /**
+     * @author Niko Toro, Grace Ordonez, Olivia Breda
+     * This class establishes a ButtonListener object that tells JButtons how they should behave once clicked.
+     */
     class ButtonListener implements ActionListener {
 
-        private Question question;
-        private JButton button;
+        private final Question question;
+        private final JButton button;
 
+        /**
+         * Non-default constructor that instantiates a JButton object and the Question object that it represents.
+         * @param question Question object representing the desired question
+         * @param button JButton object representing the JButton linked to the Question object
+         */
         public ButtonListener(Question question, JButton button) {
             this.question = question;
             this.button = button;
         }
 
+        /**
+         * Overridden actionPerformed method that creates a new AskWindow object upon a JButton being clicked,
+         * and disabling the clicked JButton to avoid repeats.
+         * @param e ActionEvent representing the clicking of a JButton
+         */
+        @Override
         public void actionPerformed(ActionEvent e) {
 
-            AskWindow aw = new AskWindow(question, players);
+            new AskWindow(question, players);
 
             ActionListener[] a = button.getActionListeners();
             button.removeActionListener((ActionListener) Array.get(a, 0));
--- src/com/company/RoundType.java
+++ src/com/company/RoundType.java
@@ -3,7 +3,6 @@ package com.company;
 /**
  * enum for rounds
  */
-
 public enum RoundType {
     Jeopardy,
     Double
